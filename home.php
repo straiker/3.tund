c7729b3a3f5817b5e001d676c5e3b582d857b55d
raitavastu <ravastu@tlu.ee>
2015-09-16 10:59:54 +0300

    commiting empty files

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rait
+ * Date: 16/09/15
+ * Time: 10:56
+ */
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rait
+ * Date: 16/09/15
+ * Time: 10:57
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>3.tund</title>
+        <meta charset="UTF-8">
+
+    </head>
+</html>
